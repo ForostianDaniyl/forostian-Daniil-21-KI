d0b2054472265be367bd4eced761027443913fe5
forostyan daniiia <uchb0vy.fordaniyila@gmail.com>
2026-09-08 23:25:54 +0300

    Add files via upload

---
+++ лаба1.py
@@ -0,0 +1,43 @@
+
+# якщо не помиляюсь то я написвав все що я пам'ятаю з курсів IT_OSVITA GRAND:(
+print("Hello World!")
+age = 17
+name = "Daniil"
+height = 1.80
+student_tfk_lntu = True
+
+x = 3
+y = 6
+a = 6
+b = 2
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(y / x)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+print(x > y)
+print(x < y)
+print(x == y)
+print(a >= b)
+print(a <= b)
+print(a != b)
+#всі опери порівняння що я пам'ятаю
+
+x = True
+y = False
+
+print("AND:", x and y)
+print("OR:", x or y)
+print("NOT:", not x)
+
+a = 5
+b = 3
+a += 2
+b *= 2
+
+print("a після +=:", a)
+print("b після *=:", b)
